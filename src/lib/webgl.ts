bf5332d62329827bcb44d1b6f58c987bf743486c
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-10-03 21:38:09 +0530

    lib/webgl.ts: format

--- src/lib/webgl.ts
+++ src/lib/webgl.ts
@@ -1,64 +1,70 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import testModel from '$lib/assets/test.glb?url';
+import testModel from '$lib/assets/testBuilding.glb?url';
 import { ThreeJSOverlayView } from '@googlemaps/three';
 import * as THREE from 'three';
 import { mapConfig } from '../utils';
 
 function initWebGLOverlay(map: google.maps.Map) {
-    let scene: THREE.Scene, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer, model, clock, loader: GLTFLoader;
-    
-    const webGLOverlayView = new google.maps.WebGLOverlayView();
-    const overlay = new ThreeJSOverlayView();
-    overlay.setAnchor(mapConfig.center)
-    
-    webGLOverlayView.onAdd = () => {
-        scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera();
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-        scene.add(ambientLight);
+	let scene: THREE.Scene,
+		camera: THREE.PerspectiveCamera,
+		renderer: THREE.WebGLRenderer,
+		model,
+		clock,
+		loader: GLTFLoader;
 
-        loader = new GLTFLoader();
-        // const source = "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
-        const source = testModel;
-        
-        loader.load(source, (gltf) => {
-            gltf.scene.scale.set(0.4,0.4,0.4);
-            gltf.scene.rotation.x = Math.PI;
-            scene.add(gltf.scene);
-        })
-    }
+	const webGLOverlayView = new google.maps.WebGLOverlayView();
+	const overlay = new ThreeJSOverlayView();
+	overlay.setAnchor(mapConfig.center);
 
-    webGLOverlayView.onContextRestored = ({ gl }) => {
-        renderer = new THREE.WebGLRenderer({ 
-            canvas: gl.canvas,
-            context: gl, ...gl.getContextAttributes()
-        });
+	webGLOverlayView.onAdd = () => {
+		scene = new THREE.Scene();
+		camera = new THREE.PerspectiveCamera();
+		const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+		scene.add(ambientLight);
 
-        renderer.autoClear = false;
+		loader = new GLTFLoader();
+		// const source = "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
+		const source = testModel;
 
-        loader.manager.onLoad = () => {
-            webGLOverlayView.requestRedraw();
-        }
-    }
+		loader.load(source, (gltf) => {
+			gltf.scene.scale.set(0.24, 0.24, 0.24);
+			gltf.scene.rotation.x = Math.PI;
+			scene.add(gltf.scene);
+		});
+	};
 
-    webGLOverlayView.onDraw = ({ gl, transformer }) => {
-        const latlng: google.maps.LatLngAltitude = {
-            //@ts-ignore
-            lat: mapConfig.center.lat,
-            //@ts-ignore
-            lng: mapConfig.center.lng,
-            altitude: 0
-        }
+	webGLOverlayView.onContextRestored = ({ gl }) => {
+		renderer = new THREE.WebGLRenderer({
+			canvas: gl.canvas,
+			context: gl,
+			...gl.getContextAttributes()
+		});
 
-        const matrix = transformer.fromLatLngAltitude(latlng);
-        camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+		renderer.autoClear = false;
 
-        webGLOverlayView.requestRedraw();
-        renderer.render(scene, camera);
-        renderer.resetState();
-    }
+		loader.manager.onLoad = () => {
+			webGLOverlayView.requestRedraw();
+		};
+	};
 
-    webGLOverlayView.setMap(map);
+	webGLOverlayView.onDraw = ({ gl, transformer }) => {
+		const latlng: google.maps.LatLngAltitude = {
+			//@ts-ignore
+			lat: mapConfig.center.lat,
+			//@ts-ignore
+			lng: mapConfig.center.lng,
+			altitude: 0
+		};
+
+		const matrix = transformer.fromLatLngAltitude(latlng);
+		camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+
+		webGLOverlayView.requestRedraw();
+		renderer.render(scene, camera);
+		renderer.resetState();
+	};
+
+	webGLOverlayView.setMap(map);
 }
 
-export { initWebGLOverlay }
+export { initWebGLOverlay };
