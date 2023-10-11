a46606d9ec9f0277c882edae95f6bb8af5ff2342
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-10-11 12:58:32 +0530

    asstes/index: remove position rotation props
    fixed cause of: bad8c66
    setting x,y,z to non zero makes models float and also we cant them from code

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,16 +1,5 @@
 import data from '$lib/data_f.json';
-
-interface position {
-    x: number;
-    y: number;
-    z: number;
-}
-
-interface rotation {
-    x: number;
-    y: number;
-    z: number;
-}
+import type { Vector3Tuple } from 'three';
 
 interface LatLongLiteral {
     lat: number;
@@ -19,58 +8,54 @@ interface LatLongLiteral {
 }
 
 type ModelProps = {
-    position: position;
-    rotation: rotation;
+    scale: Vector3Tuple;
     LatLongLiteral?: LatLongLiteral;
 }
 
+type Models = {
+    [key: string]: {
+        model: string;
+        props: ModelProps;
+    }
+}
+
 function gen_ModelProps(
-    position: position, rotation: rotation, latlong: LatLongLiteral
+    scale: Vector3Tuple,
+    latlong: LatLongLiteral
     ): ModelProps {
     
     return {
-        position: {
-            x: position.x,
-            y: position.y,
-            z: position.z
-        },
-        rotation: {
-            x: rotation.x,
-            y: rotation.y,
-            z: rotation.z
-        },
+        scale: scale,
         LatLongLiteral: {
             lat: latlong.lat,
             lng: latlong.lng,
-            altitude: 10,
+            altitude: 0,
         }
     }
 }
 
 import ADMIN_BLOCK from '$lib/assets/ab-block.glb?url';
-import TAGORE_A from '$lib/assets/Tagore Bhavan - A.glb?url';
+import TAGORE_A from '$lib/assets/Teresa - A.glb?url';
 import TAGORE_B from '$lib/assets/Tagore Bhavan-B.glb?url';
+import PIMSR_LIB from '$lib/assets/PIMSR Library.glb?url';
 
-let ADMIN_BLOCK_PROPS   = gen_ModelProps({x: 7, y: 0, z: -10}, {x: 0, y: 0, z: 0}, data['PARUL ADMISSION CELL']);
-let TAGORE_A_PROPS      = gen_ModelProps({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, data['TAGORE BHAWAN - A']);
-let TAGORE_B_PROPS      = gen_ModelProps({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, data['TAGORE BHAWAN - B']);
+let ADMIN_BLOCK_PROPS   = gen_ModelProps([40, 40, 40], data['PARUL ADMISSION CELL']);
+let TAGORE_B_PROPS      = gen_ModelProps([15, 15, 15], data['TAGORE BHAWAN - B']);
+let PIMSR_LIB_PROPS     = gen_ModelProps([15, 15, 15], data['PIMSR LIBRARY']);
 
-const MODELS = {
-    /// all models in one array
-    "ALL_MODELS": [ADMIN_BLOCK, TAGORE_A],
-
+const MODELS: Models = {
     "PARUL ADMISSION CELL": {
         model: ADMIN_BLOCK,
         props: ADMIN_BLOCK_PROPS
     },
-    "TAGORE BHAWAN - A": {
-        model: TAGORE_A,
-        props: TAGORE_A_PROPS
-    },
     "TAGORE BHAWAN - B": {
         model: TAGORE_B,
         props: TAGORE_B_PROPS
+    },
+    "PIMSR LIBRARY": {
+        model: PIMSR_LIB,
+        props: PIMSR_LIB_PROPS
     }
 }
 
-export { MODELS };
+export { MODELS, type ModelProps };
